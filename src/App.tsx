dc07c66eb59f33510091845896f1633d90dc4731
Andrés Fernández <andres@MacBook-Pro-de-Andres.local>
2024-11-08 23:01:49 -0300

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable react-hooks/rules-of-hooks */
+
 import { useEffect, useMemo, useReducer } from "react"
 import Form from "./components/Form"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
